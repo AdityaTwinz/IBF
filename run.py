7d133b366e9aece345fe44955fe430a1873b1750
[ XCTeamCode - ( Aditya Putra Mahesa XD  ) ] - <136499834+AdityaTwinz@users.noreply.github.com>
2023-12-10 10:44:58 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+
+def Menuuu():
+    os.system('clear')
+    print(' [+] Script sedang dalam perbaikan oleh admin\n [+] Mohon tunggu sampai selesai ya')
+    os.system('xdg-open https://wa.me/+6283861183874')
+
+os.system('git pull') ; Menuuu()
